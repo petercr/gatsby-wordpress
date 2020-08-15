2a2866d433dcc2b8ee05a409b8cc43eab3fe7eaf
Peter <petecruckshank@gmail.com>
2020-08-14 21:35:51 -0400

    added a `createsPages` call to make a page 4 posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,33 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// Dropped in code from gatsby-source-wordpress-experimental
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWpPost(sort: { fields: [date] }) {
+        nodes {
+            title
+            excerpt
+            content
+            slug
+        }
+      } 
+    }
+  `).then(result => {
+    //highlight-start
+    result.data.allWpPost.nodes.forEach((node) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+    //highlight-end
+  })
+}
